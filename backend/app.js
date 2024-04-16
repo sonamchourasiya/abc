3671f77e3c1ebff9b9d8c2f90edff13270f128aa
sonamchourasiya <sonamchourasiya709@gmail.com>
2024-04-16 19:15:11 +0530

    Mongodb connection added

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-mongoose.connect('mongodb://localhost:27017/your_database_name', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/mydb', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
   })
   .catch((err) => {
     console.error('Error connecting to MongoDB:', err);
   });
+
+const userSchema = new Schema({
+  ts: String,
+  machine_status: Number,
+  vibration: Number
+});
+
+const User = mongoose.model('User', userSchema);
